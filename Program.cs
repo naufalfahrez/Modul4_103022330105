d4ff7c4d3d44c86003aa21840b59bd243e034874
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 17:11:14 +0700

    menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 class KodeProduk {
     public enum Produk {
@@ -22,6 +23,67 @@ class KodeProduk {
         return kodproduk[(int) elektronik];
     }
 
+    internal class FanLaptop
+    {
+        public enum FanState { Quite, Balance, Performance, Turbo }
+        public enum Trigger { modeDown, modeUp, turboShortcut }
+
+        class Transition
+        {
+            public FanState prevstate;
+            public FanState nextstate;
+            public Trigger trigger;
+            public Transition(FanState prevstate, FanState nextstate, Trigger trigger)
+            {
+                this.prevstate = prevstate;
+                this.nextstate = nextstate;
+                this.trigger = trigger;
+            }
+
+            private FanState currentState;
+            public FanLaptop()
+            {
+                currentState = FanState.Quite;
+            }
+
+            public Static List<Transition>transitions = new List<Transition>
+            {
+                new Transition(FanState.Quite, FanState.Balance, Trigger.modeUp),
+                new Transition(FanState.Quite, FanState.Turbo, Trigger.turboShortcut),
+                new Transition(FanState.Balance, FanState.Quite, Trigger.modeDown),
+                new Transition(FanState.Balance, FanState.Performance, Trigger.modeUp),
+                new Transition(FanState.Performance, FanState.Balance, Trigger.modeDown),
+                new Transition(FanState.Performance, FanState.Turbo, Trigger.modeUp),
+                new Transition(FanState.Turbo, FanState.Performance, Trigger.modeDown),
+                new Transition(FanState.Turbo, FanState.Quite, Trigger.turboShortcut),
+
+            };
+
+            public FanState GetNextState(FanState prevstate, FanState nextstate, Trigger trigger)
+            {
+                foreach (var transition in transition)
+                {
+                    if (transition.prevstate == prevstate && transition.trigger == trigger)
+                    {
+                        return transition.currentState;
+                    }
+                    return prevstate;
+                }
+                Public void ActiveTrigger(Trigger trigger)
+                {
+                    FanState nextState = GetNextState(currentState, trigger);
+                    if (nextState != currentState)
+                    {
+                        currentState = nextState;
+                        Console.WriteLine(nextState == FanState.Quite ? "Fan quite berubah menjadi turbo")
+                    }
+                }
+            }
+        }
+    }
+
+
+
     public static void Main()
     {
         Console.WriteLine("kode produk Laptop: " + KodeProduk.getKodeProduk(KodeProduk.Produk.Laptop));
@@ -34,7 +96,19 @@ class KodeProduk {
         Console.WriteLine("kode produk Monitor: " + KodeProduk.getKodeProduk(KodeProduk.Produk.Monitor));
         Console.WriteLine("kode produk Smartwatch: " + KodeProduk.getKodeProduk(KodeProduk.Produk.Smartwatch));
         Console.WriteLine("kode produk Kamera: " + KodeProduk.getKodeProduk(KodeProduk.Produk.Kamera));
+
+        FanLaptop fan = new FanLaptop();
+        while (true) {
+            Console.WriteLine("Masukkan perintah(modeDown/mopdeUp/turboshortcut): ");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "modeUp") {
+                fan.ActivateTrigger(Trigger)
+            }
+        }
+
     }
+
 }
 
 
